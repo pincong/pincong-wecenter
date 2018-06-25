b853e0216a459f203af54d21669f7d3553e1aaa4
name <email>
2018-06-25 03:08:03 +0000

    base url

--- system/functions.inc.php
+++ system/functions.inc.php
@@ -31,8 +31,8 @@ function base_url()
 {
 	$clean_url = isset($_SERVER['PATH_INFO']) ? $_SERVER['PATH_INFO'] : NULL;
 	$clean_url = dirname(rtrim($_SERVER['PHP_SELF'], $clean_url));
-	$clean_url = rtrim($_SERVER['HTTP_HOST'] . $clean_url, '/\\');
-
+	//$clean_url = rtrim($_SERVER['HTTP_HOST'] . $clean_url, '/\\');
+	return $clean_url;
 	if ((isset($_SERVER['HTTPS']) AND !in_array(strtolower($_SERVER['HTTPS']), array('off', 'no', 'false', 'disabled'))) OR $_SERVER['SERVER_PORT'] == 443)
 	{
 		$scheme = 'https';
