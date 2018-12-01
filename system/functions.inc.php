151ae4a725bceed9ebd3815b38186cc94700318d
name <email>
2018-12-01 00:00:00 +0000

    由于BUG不再处理全角空格

--- system/functions.inc.php
+++ system/functions.inc.php
@@ -1138,10 +1138,11 @@ function future_time()
 
 function my_trim($str)
 {
+	// trim('【BUG】trim 函数处理全角空格会产生 bug 会清除整个字符串', "　");
+
 	// 0-31
 	$whitespace = "\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0A\x0B\x0C\x0D\x0E\x0F\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F"
 	. " "		// U+0020 SPACE
-	. " "		// U+00A0 NO-BREAK SPACE
-	. "　";		// 全角空格
-    return trim($str, $whitespace);
+	. " ";		// U+00A0 NO-BREAK SPACE
+	return trim($str, $whitespace);
 }
