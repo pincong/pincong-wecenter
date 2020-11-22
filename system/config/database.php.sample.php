ab8a62692ead82564439d279e632c41565a6f9f1
pincong-voting-test <62181719+pincong-voting-test@users.noreply.github.com>
2020-11-22 00:16:06 -0500

    Create database.php.sample.php

---
+++ system/config/database.php.sample.php
@@ -0,0 +1,13 @@
+<?php
+
+$config['charset'] = 'utf8mb4';
+$config['prefix'] = 'aws_';
+$config['driver'] = 'PDO_MYSQL';
+$config['master'] = array (
+  'charset' => 'utf8mb4',
+  'host' => 'localhost',
+  'username' => 'root',
+  'password' => '',
+  'dbname' => 'pincong',
+);
+$config['slave'] = false;
