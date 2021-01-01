c80ba3e4bcb58817a3f638d949130baa2256324f
name <email>
2021-01-01 00:00:00 +0000

    修复settings.sql当用户组id是0时产生的bug

---
+++ install/db/admin.sql
@@ -0,0 +1,5 @@
+-- --------------------------------------------------------
+
+
+INSERT INTO `aws_users` (`user_name`, `password`, `salt`, `group_id`) VALUES
+	('admin', '$2y$10$TaEOhERXEB51Ds8i/iBbq.Qy2v82s3yidCLGz/iv25PJ7hxXnq7r.', 'gzeu', 1);
