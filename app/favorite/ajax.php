0d9748d2bbbe7d86029363edf4574f7a43803e4b
name <email>
2021-01-01 00:00:00 +0000

    恢復收藏功能

--- app/favorite/ajax.php
+++ app/favorite/ajax.php
@@ -26,7 +26,7 @@ class ajax extends AWS_CONTROLLER
 	{
 		H::no_cache_header();
 	}
-/*
+
 	public function add_favorite_action()
 	{
 		if (!$this->model('favorite')->check_item_type(H::POST('item_type')))
@@ -39,9 +39,17 @@ class ajax extends AWS_CONTROLLER
 			H::ajax_error((_t('操作过于频繁, 请稍后再试')));
 		}
 
-		if (!$this->model('content')->get_thread_or_reply_info_by_id(H::POST('item_type'), H::POST('item_id')))
+		if (!$item_info = $this->model('content')->get_thread_or_reply_info_by_id(H::POST('item_type'), H::POST('item_id')))
 		{
 			H::ajax_error((_t('内容不存在')));
+		}
+
+		if ($item_info['uid'] != $this->user_id)
+		{
+			if (!$this->user_info['permission']['follow_thread'])
+			{
+				H::ajax_error((_t('你的声望还不够')));
+			}
 		}
 
 		set_user_operation_last_time('favorite', $this->user_id);
@@ -50,7 +58,7 @@ class ajax extends AWS_CONTROLLER
 
 		H::ajax_success();
 	}
-*/
+
 	public function remove_favorite_item_action()
 	{
 		if (!$this->model('favorite')->check_item_type(H::POST('item_type')))
